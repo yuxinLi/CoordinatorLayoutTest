6a334a18635eac69ec54e2f984cfb91efab1b88f
liyuxin <liyx@13322.com>
2017-08-24 17:34:25 +0800

    test

---
+++ nestedscrolltest/src/main/java/com/example/nestedscrolltest/NestedChildLayout.java
@@ -0,0 +1,139 @@
+package com.example.nestedscrolltest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.support.annotation.Size;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.NestedScrollingChildHelper;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * 描    述：
+ * 作    者：liyx@13322.com
+ * 时    间：2017/8/24
+ */
+
+public class NestedChildLayout extends View implements NestedScrollingChild{
+
+    NestedScrollingChildHelper mNestedScrollingChildHelper;
+    Paint mPaint;
+    int mDownX , mDownY;
+    int[] mConsumed , mOffsetInWindow;
+
+    public NestedChildLayout(Context context) {
+        this(context , null);
+    }
+
+    public NestedChildLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs , 0);
+    }
+
+    public NestedChildLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mNestedScrollingChildHelper = new NestedScrollingChildHelper(this);
+        mNestedScrollingChildHelper.setNestedScrollingEnabled(true);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLUE);
+
+        mConsumed = new int[2];
+        mOffsetInWindow = new int[2];
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(getWidth() / 2 , getHeight() / 2 , getWidth() / 2 , mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mDownX = x;
+                mDownY = y;
+                startNestedScroll(ViewCompat.SCROLL_AXIS_HORIZONTAL | ViewCompat.SCROLL_AXIS_VERTICAL);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                int dx = x - mDownX;
+                int dy = y - mDownY;
+
+                if (dispatchNestedPreScroll(dy , dy , mConsumed , mOffsetInWindow)){
+                    dx = dx - mConsumed[0];
+                    dy = dy - mConsumed[1];
+                }
+                offsetLeftAndRight(dx);
+                offsetTopAndBottom(dy);
+
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                stopNestedScroll();
+                break;
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public void setNestedScrollingEnabled(boolean enabled) {
+        mNestedScrollingChildHelper.setNestedScrollingEnabled(enabled);
+    }
+
+    @Override
+    public boolean isNestedScrollingEnabled() {
+        return mNestedScrollingChildHelper.isNestedScrollingEnabled();
+    }
+
+    @Override
+    public boolean startNestedScroll(int axes) {
+        return mNestedScrollingChildHelper.startNestedScroll(axes);
+    }
+
+    @Override
+    public void stopNestedScroll() {
+        mNestedScrollingChildHelper.stopNestedScroll();
+    }
+
+    @Override
+    public boolean hasNestedScrollingParent() {
+        return mNestedScrollingChildHelper.hasNestedScrollingParent();
+    }
+
+    @Override
+    public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
+        return mNestedScrollingChildHelper.dispatchNestedPreFling(velocityX , velocityY);
+    }
+
+    @Override
+    public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
+        return mNestedScrollingChildHelper.dispatchNestedFling(velocityX , velocityY , consumed);
+    }
+
+    @Override
+    public boolean dispatchNestedPreScroll(int dx, int dy, @Nullable @Size(value = 2) int[] consumed, @Nullable @Size(value = 2) int[] offsetInWindow) {
+        return mNestedScrollingChildHelper.dispatchNestedPreScroll(dx , dy , consumed , offsetInWindow);
+    }
+
+    @Override
+    public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, @Nullable @Size(value = 2) int[] offsetInWindow) {
+        return mNestedScrollingChildHelper.dispatchNestedScroll(dxConsumed , dyConsumed , dxUnconsumed , dyUnconsumed , offsetInWindow);
+    }
+}
---
+++ nestedscrolltest/src/main/java/com/example/nestedscrolltest/NestedParentLayout.java
@@ -0,0 +1,107 @@
+package com.example.nestedscrolltest;
+
+import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.NestedScrollingParentHelper;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * 描    述：
+ * 作    者：liyx@13322.com
+ * 时    间：2017/8/24
+ */
+
+public class NestedParentLayout extends FrameLayout implements NestedScrollingParent {
+
+    NestedScrollingParentHelper mNestedScrollingParentHelper;
+
+    public NestedParentLayout(@NonNull Context context) {
+        this(context , null);
+    }
+
+    public NestedParentLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs , 0);
+    }
+
+    public NestedParentLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mNestedScrollingParentHelper = new NestedScrollingParentHelper(this);
+    }
+
+
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(View child, View target, int axes) {
+        mNestedScrollingParentHelper.onNestedScrollAccepted(child , target , axes);
+    }
+
+    @Override
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        super.onNestedPreScroll(target, dx, dy, consumed);
+
+        if (dx > 0){
+            if (target.getRight() + dx > getWidth()){
+                consumed[0] = target.getRight() + dx - getWidth();
+                offsetLeftAndRight(consumed[0]);
+            }
+        }else {
+            if (target.getLeft() + dx < 0){
+                consumed[0] = target.getLeft() + dx;
+                offsetLeftAndRight(consumed[0]);
+            }
+        }
+
+        if (dy > 0){
+            if (target.getBottom() + dy > getHeight()){
+                consumed[1] = target.getBottom() + dy - getHeight();
+                offsetTopAndBottom(consumed[1]);
+            }
+        }else {
+            if (target.getTop() + dy < 0){
+                consumed[1] += target.getTop() + dy;
+                offsetTopAndBottom(consumed[1]);
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+    }
+
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        return super.onNestedPreFling(target, velocityX, velocityY);
+    }
+
+    @Override
+    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+        return super.onNestedFling(target, velocityX, velocityY, consumed);
+    }
+
+
+    @Override
+    public void onStopNestedScroll(View child) {
+        mNestedScrollingParentHelper.onStopNestedScroll(child);
+    }
+
+    @Override
+    public int getNestedScrollAxes() {
+        return mNestedScrollingParentHelper.getNestedScrollAxes();
+    }
+}
